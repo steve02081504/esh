1b02732a23bf6c457341947164304044e8eeee04
steve02081504 <steve02081504@EDEN.fukwold>
2023-05-04 18:42:38 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,9 +7,17 @@ function prompt {"$PWD>"}
 
 #set the title same as cmd
 $host.UI.RawUI.WindowTitle = "命令提示符"
+#if as root
+If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”)){
+    $host.UI.RawUI.WindowTitle += "(root)"
+}
+
 
 #remove alias "rm" as it is conflict with linux submode's /usr/bin/rm
 Remove-Item -Path Alias:rm
+
+#import appx with -UseWindowsPowerShell to avoid [Operation is not supported on this platform. (0x80131539)]
+Import-Module Appx -UseWindowsPowerShell
 
 #clear screen
 Clear-Host
