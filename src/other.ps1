2e496ee20085bd952e6d03e8d8e7b446652bcfca
steve02081504 <steve02081504@EDEN.fukwold>
2023-11-09 23:10:34 +0800

    copilot

--- src/other.ps1
+++ src/other.ps1
@@ -298,6 +298,47 @@ function Update-EShell {
 	catch {}
 }
 
+function Install-Copilot {
+	if (-not (Test-Command gh)) {
+		#github cli not found
+		if (Test-Command winget) {
+			try {
+				winget install GitHub.cli
+				gh extension install github/gh-copilot
+			} catch {
+				#install failed
+				Write-Error "Error: Install github cli failed."
+				throw
+			}
+		}
+		else {
+			#winget not found
+			Write-Error "Please install github cli first."
+			throw
+		}
+	}
+}
+
+function Copilot {
+	try { Install-Copilot } catch { return }
+	gh copilot suggest -t shell @args
+}
+
+function Copilot.GitHub {
+	try { Install-Copilot } catch { return }
+	gh copilot suggest -t gh @args
+}
+
+function Copilot.Git {
+	try { Install-Copilot } catch { return }
+	gh copilot suggest -t git @args
+}
+
+function Copilot.Explain {
+	try { Install-Copilot } catch { return }
+	gh copilot explain @args
+}
+
 function Update-All-Paks {
 	if (Test-Command pip) {
 		Write-Host "Updating python packages..."
