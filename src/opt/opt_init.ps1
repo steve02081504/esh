c136da139b4c3fd6d8187b2be6d5f2ff7d79c439
steve02081504 <steve02081504@EDEN.fukwold>
2024-01-05 22:45:43 +0800

    Update opt_init.ps1

--- src/opt/opt_init.ps1
+++ src/opt/opt_init.ps1
@@ -1,13 +1,6 @@
 if((Get-ExecutionPolicy) -eq 'Restricted'){ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force }
-# 遍历环境变量
-$env:Path.Split(";") | ForEach-Object {
-	if ($_ -and (-not (Test-Path $_ -PathType Container))) {
-		Write-Warning "检测到无效的环境变量于$_，请考虑删除"
-	}
-	elseif ($_ -like "*[\\/]esh[\\/]path*") {
-		$Script:eshDir = $_ -replace "[\\/]path[\\/]*$", ''
-		$Script:eshDirFromEnv = $true
-	}
+if ($Script:eshDirFromEnv = Get-Command esh -ErrorAction Ignore) {
+	$Script:eshDir = Split-Path $(Split-Path $eshDirFromEnv.Source)
 }
 # 使用if判断+赋值：我们不能使用??=因为用户可能以winpwsh运行该脚本
 if (-not $eshDir) {
