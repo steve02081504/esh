055c9633a045f8cd58538ad15991851485b0d711
steve02081504 <steve02081504@EDEN.fukwold>
2024-05-27 10:28:21 +0800

    l

--- src/commands/hongmenglike.ps1
+++ src/commands/hongmenglike.ps1
@@ -1,18 +1,20 @@
-if (Test-Command git.exe) {
-	function global:git {
-		if ($args -eq '-v') {
-			$result = git.exe $args
-			$result -replace '.windows', '.EDENOS'
-		}
-		else {
-			git.exe $args
+if ($env:EdenOS) {
+	if (Test-Command git.exe) {
+		function global:git {
+			if ($args -eq '-v') {
+				$result = git.exe $args
+				$result -replace '.windows', '.EDENOS'
+			}
+			else {
+				git.exe $args
+			}
 		}
 	}
-}
 
-if (Test-Command uname.exe) {
-	function global:uname {
-		$result = uname.exe $args
-		$result -ireplace 'MSYS_NT', 'EDENOS' -ireplace 'Msys', 'eden'
+	if (Test-Command uname.exe) {
+		function global:uname {
+			$result = uname.exe $args
+			$result -ireplace 'MSYS_NT', 'EDENOS' -ireplace 'Msys', 'eden'
+		}
 	}
 }
