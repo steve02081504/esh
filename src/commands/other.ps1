2451fb2af41201ede79c75528664e9ed72f2f2c9
steve02081504 <steve02081504@EDEN.fukwold>
2025-11-03 14:26:11 +0800

    better `Get-Promptlized-Dir`

--- src/commands/other.ps1
+++ src/commands/other.ps1
@@ -693,7 +693,7 @@ function global:Get-Promptlized-Dir(
 	$Path = '.',
 	$Recurse = $true
 ) {
-	$str = (Get-ChildItem -Recurse:$Recurse -Path $Path -Filter $Filter -Include $Include -Exclude $Exclude -File | ForEach-Object {
+	$str = (Get-ChildItem -Recurse:$Recurse -Path $Path -Filter $Filter -Include $Include -Exclude $Exclude -File | Where-Object { $_.Length -le 3MB } | ForEach-Object {
 		$content = (Get-Content $_) -join "`n"
 		if ($content.IndexOf([byte]0) -ne -1) { return }
 		$quote = '```'
